8d7f60d8364dcaea8e3b826afc817e354d8d6992
mazeltov123 <104516594+mazeltov123@users.noreply.github.com>
2026-04-12 14:26:42 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
